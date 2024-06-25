872844059ba6029cb114bdf48ee50deea8ea4754
Ansak Mahir <ansarmmm.21@uom.lk>
2024-06-25 15:36:12 +0530

    check finished

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboar';
+    public const HOME = '/dashboard';
     public const STUDENT_HOME = '/Student';
 
     /**
