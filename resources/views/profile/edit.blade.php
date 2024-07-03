32fef8ffaca1db621cede04b53b57c712d0ca34d
KSaubhagya <saubhagyakrhmk.21@uom.lk>
2024-07-03 08:26:32 +0530

    profile pic

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -76,8 +76,14 @@
 
         <div class="row">
             <div class="col-md-3 text-center">
-                <img src="https://via.placeholder.com/80" class="profile-photo" alt="Profile Photo">
-                <button class="btn btncolor change-photo-btn">Change Photo</button>
+                <img src="https://via.placeholder.com/80" class="profile-photo" alt="Profile Photo" id="profile-photo">
+                <form method="post" action="{{ route('profile.photo.update') }}" enctype="multipart/form-data">
+                    @csrf
+                    @method('patch')
+                    <input type="file" class="form-control d-none" id="photo" name="photo" accept="image/*">
+                    <button type="button" class="btn btncolor change-photo-btn" id="change-photo-btn">Change Photo</button>
+                    <button type="submit" class="btn btncolor mt-2 d-none" id="upload-photo-btn">Upload Photo</button>
+                </form>
             </div>
             <div class="col-md-9">
                 <form method="post" action="{{ route('profile.update') }}">
@@ -140,5 +146,21 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+<script>
+    document.getElementById('change-photo-btn').addEventListener('click', function() {
+        document.getElementById('photo').click();
+    });
+
+    document.getElementById('photo').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                document.getElementById('profile-photo').src = e.target.result;
+            }
+            reader.readAsDataURL(this.files[0]);
+            document.getElementById('upload-photo-btn').classList.remove('d-none');
+        }
+    });
+</script>
 </body>
 </html>
